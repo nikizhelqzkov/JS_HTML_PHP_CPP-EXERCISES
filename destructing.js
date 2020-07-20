01f5d22d631608852515bca0dfab495700d7015c
Nikolay Zhelyazkov <nikizhelqzkov@gmail.com>
2020-07-20 18:00:10 +0300

    example of destructing

---
+++ destructing.js
@@ -0,0 +1,8 @@
+const things = function(...arr) {
+    const [love, favorCity, favNumber] = arr;
+    console.log('My love is : ' + love);
+    console.log('My favorite city is: ' + favorCity);
+    console.log(`My favorite number is: ${favNumber}`);
+}
+
+things('Sofi', 'Sofia', '18');
