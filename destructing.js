73364df88c41e74f2f2fb5837b48a9ee45f952c2
Nikolay Zhelyazkov <nikizhelqzkov@gmail.com>
2020-07-20 18:20:19 +0300

    adding more

--- destructing.js
+++ destructing.js
@@ -6,3 +6,20 @@ const things = function(...arr) {
 }
 
 things('Sofi', 'Sofia', '18');
+
+const Bulgaria = {
+    capital: 'Sofia',
+    population: 6900000,
+    president: 'Rumen Radev',
+    primeMinister: 'Boiko Borisov'
+}
+const { capital, population, president, primeMinister } = Bulgaria;
+
+const ob = function(...names) {
+    const [cap, pop, pres, prM] = names;
+    console.log(`Our Capital is : ${cap}`);
+    console.log(`Our Population is: ${pop}`);
+    console.log(`Our president is: ${pres}`);
+    console.log(`Our prime minister is: ${prM}`);
+}
+ob(capital, population, president, primeMinister);
