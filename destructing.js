456d39dcf9e139e75754c738c0f69cee0d5a9144
Nikolay Zhelyazkov <nikizhelqzkov@gmail.com>
2020-07-20 18:01:47 +0300

    edit

--- destructing.js
+++ destructing.js
@@ -1,7 +1,7 @@
 const things = function(...arr) {
     const [love, favorCity, favNumber] = arr;
-    console.log('My love is : ' + love);
-    console.log('My favorite city is: ' + favorCity);
+    console.log(`My love is : ${love}`);
+    console.log(`My favorite city is: ${favorCity}`);
     console.log(`My favorite number is: ${favNumber}`);
 }
 
