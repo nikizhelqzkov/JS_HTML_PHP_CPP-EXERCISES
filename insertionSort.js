f10356fa83cb0c738423c465343fb412312c6a4f
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-03-12 01:25:50 +0200

    insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,16 @@
+let arr = [1,2,5,2,5,2,45,41,4,2,5,41,2,54];
+const insertionSort = (arr) =>{
+    for (let i = 1; i < arr.length; ++i) {
+        let element = arr[i];
+        let index = i-1;
+        while(index>=0 && arr[index]>element){
+            arr[index+1] = arr[index];
+            --index;
+        }
+        arr[index+1] = element;
+    }
+    
+}
+
+insertionSort(arr);
+console.log(...arr)
