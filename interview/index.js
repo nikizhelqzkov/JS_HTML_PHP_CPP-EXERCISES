b449308d1705126778477a161929e7ea134d3bec
nikizhelqzkov <nikizhelqzkov@gmail.com>
2022-01-14 17:36:29 +0200

    exercise for nemetcheck inteview

--- interview/index.js
+++ interview/index.js
@@ -19,14 +19,6 @@ class Bus {
 
   //weight, name, fuelType, capacity
 }
-const bus = new Bus(5000, "Mercedes", "Diesel", 50);
-try {
-  const hasFree = bus.hasFreeSeets();
-  ``;
-} catch (error) {
-  console.log(error);
-}
-// console.log(bus.hasFreeSeets());
 class Train {
   constructor(weight, name, parts, capacity) {
     this.weight = weight;
@@ -45,6 +37,10 @@ class Train {
       console.log(error);
     }
   }
-
-  //weight, name, fuelType, capacity
+}
+const bus = new Bus(5000, "Mercedes", "Diesel", 50);
+try {
+  const hasFree = bus.hasFreeSeets();
+} catch (error) {
+  console.log(error);
 }
