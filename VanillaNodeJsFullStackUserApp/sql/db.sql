99246ee6bb1cd4f3c2410bb3e4485b18c5160b47
nikizhelqzkov <nikizhelqzkov@gmail.com>
2024-06-22 02:58:21 +0300

    added db script

---
+++ VanillaNodeJsFullStackUserApp/sql/db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS userdb;
+USE userdb;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
